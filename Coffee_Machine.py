3d9b9e6bc04814b725f7ef983d9dad0b14a13259
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2023-12-05 15:46:31 -0800

    finish

--- Coffee_Machine.py
+++ Coffee_Machine.py
@@ -21,94 +21,59 @@ MENU = {
             "coffee": 24,
         },
         "cost": 3.0,
-    }
+    },
 }
-make_coffee = False
-total_bill = []
-while not make_coffee:
-    order = input("What would you like? (espresso, latte, or cappuccino?) ").lower()
-    if order == "espresso":
-        bill = 1.5
-        total_bill.append(bill)
-    elif order == "latte":
-        bill = 2.5
-        total_bill.append(bill)
-    elif order == "cappuccino":
-        bill = 3.0
-        total_bill.append(bill)
-    elif order == "off": 
-        break
 
-    print("Please insert coins.")
-
-    quarters = int(input("How many quarters? "))
-    dimes = int(input("How many dimes? "))
-    nickles = int(input("How many nickles? "))
-    pennies = int(input("How many pennies? "))
-    total_coins = ((quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01))
-    total = sum(total_bill)
-
-    print(total, total_coins)
-    
-    if total_coins < total:
-        print("Sorry, that's not enough money. Money refunded.")
-    elif total_coins > total:
-        change = total_coins - total
-        print(f"Here is ${change} in change.")
-        print(f"Here is your {order} ☕. Enjoy!")
-
-# TODO 3. Print report.
-# a. When the user enters “report” to the prompt, a report should be generated that shows
-# the current resource values. e.g.
 reserve = {
     "water": 100,
     "milk": 50,
     "coffee": 76,
     "money": 2.5,
 }
-# TODO 4. Check resources sufficient?
-# a. When the user chooses a drink, the program should check if there are enough
-# resources to make that drink.
-# b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
-# not continue to make the drink but print: “Sorry there is not enough water.”
-# c. The same should happen if another resource is depleted, e.g. milk or coffee.
-# TODO 5. Process coins.
-# a. If there are sufficient resources to make the drink selected, then the program should
-# prompt the user to insert coins.
-# b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
-# c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
-# pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
-# TODO 6. Check transaction successful?
-# a. Check that the user has inserted enough money to purchase the drink they selected.
-# E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the
-# program should say “Sorry that's not enough money. Money refunded.”.
-# b. But if the user has inserted enough money, then the cost of the drink gets added to the
-# machine as the profit and this will be reflected the next time “report” is triggered. E.g.
-# Water: 100ml
-# Milk: 50ml
-# Coffee: 76g
-# Money: $2.5
-# c. If the user has inserted too much money, the machine should offer change.
-# E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal
-# places.
-# TODO 7. Make Coffee.
-# a. If the transaction is successful and there are enough resources to make the drink the
-# user selected, then the ingredients to make the drink should be deducted from the
-# coffee machine resources.
-# E.g. report before purchasing latte:
-# Water: 300ml
-# Milk: 200ml
-# Coffee: 100g
-# Money: $0
-# Report after purchasing latte:
-# Water: 100ml
-# Milk: 50ml
-# Coffee: 76g
-# Money: $2.5
-# b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If
-# latte was their choice of drink
 
+def report():
+    print(f"Water: {reserve['water']}")
+    print(f"Milk: {reserve['milk']}")
+    print(f"Coffee: {reserve['coffee']}")
+    print(f"Money: {reserve['money']}")
 
+def coins():
+    """Ask user for payment in coints and add them up"""
+    quarters = int(input("How many quarters? "))
+    dimes = int(input("How many dimes? "))
+    nickles = int(input("How many nickles? "))
+    pennies = int(input("How many pennies? "))
+    total_coins = (
+        (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
+    )
+    return total_coins
 
+def transaction(total_coins, total_bill):
+    if total_coins < total_bill:
+        print("Sorry, that's not enough money. Money refunded.")
+        return False
+    else:
+        change = total_coins - total_bill
+        print(f"Here is ${round(change, 2)} in change.")
+        print(f"Here is your {order} ☕. Enjoy!")
+        return True
 
+make_coffee = True
+total_bill = 0
+
+while make_coffee:
+    order = input(
+        "What would you like? (espresso, latte, or cappuccino?) ").lower()
+    if order == "off":
+        make_coffee = False
+    elif order == "report":
+        report()
+    else:
+        coffee = MENU[order]
+        print("Please insert coins.")
+        total_coins = coins()
+        transaction(total_coins, total_bill)
+        break
+
+reserve['money'] += total_bill
 
